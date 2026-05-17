547dc6f2da11e9116fbdbfa86f869952c7657089
Poornima Batham <pbatham21@gmail.com>
2026-05-17 18:59:58 +0530

    deploy

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,11 +1,9 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
-    // baseURL: 'https://employeemanagamentbackend-production.up.railway.app/api'
-
-  
- });
+  // baseURL: "http://localhost:5000/api",
+  baseURL: "https://employeemanagamentbackend-production.up.railway.app/api",
+});
 
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
